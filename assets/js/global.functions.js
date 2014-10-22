2a403d4aef6907ca9a669ded73bfcb1ec59c3e2f
Christopher Hacia <chris@chrishacia.com>
2014-10-22 08:27:48 -0700

    updating repo, essentially former blank_template with idea in direction of the budgets, seperating the repos as they have there own purposes

---
+++ assets/js/global.functions.js
@@ -0,0 +1,20 @@
+/*
+var myColorSwap = null;
+function startColorSwapper()
+{
+    myColorSwap = setInterval(function()
+    {
+        randomColor = Math.floor(Math.random()*16777215).toString(16);
+        $('body').css({'background-color':'#'+randomColor});
+    }, 5000);
+}
+function stopColorSwapper()
+{
+    clearInterval(myColorSwap);
+    myColorSwap = null;
+}
+$(document).ready(function()
+{
+    startColorSwapper();
+});
+*/
